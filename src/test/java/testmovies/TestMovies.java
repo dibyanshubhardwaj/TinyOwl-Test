f4a24aa64a9b8ac72d349c99cd632445370a4aa9
dibyanshubhardwaj <bhardwajdibyanshu@gmail.com>
2015-03-07 16:19:20 +0530

    tinyowl

--- src/test/java/testmovies/TestMovies.java
+++ src/test/java/testmovies/TestMovies.java
@@ -2,12 +2,16 @@ package testmovies;
 
 
 import movies.Movies;
+import movies.SqliteHandler;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -16,22 +20,31 @@ public class TestMovies {
     WebDriver driver;
     Movies movieName;
 
-    //@Test(priority = 0)
-    public void setup(){
 
+
+    @Test
+    public List<WebElement> setup(){
+    //find the element
+
+            return driver.findElements(By.xpath("//table//tbody[@class='lister-list']//tr"));
+
+
+
+
+    }
+      @BeforeMethod
+    public void beforeMethod(){
+        //initialize the firebox browser
         driver = new FirefoxDriver();
+        //declare the implicit wait time
         driver.manage().timeouts().implicitlyWait(200, TimeUnit.SECONDS);
+        //launching the browser
         System.out.println("Launching Browser");
         driver.get("http://www.imdb.com/chart/top");
 
     }
-    @Test(priority = 1)
-    public void count_in_db(){
-
-        List<WebElement> optionCount;
-        optionCount = driver.findElements(By.xpath("//table//tbody[@class='lister-list']//tr"));
-        System.out.println(optionCount.size());
-
+    @AfterMethod
+    public void afterMethod(){
+        driver.quit();
     }
-
 }
